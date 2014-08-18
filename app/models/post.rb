1650559f7b41ffd99f0815599d67606f86f69e64
David Bayon <dbayon82@googlemail.com>
2014-08-18 17:52:58 +0100

    added some basic styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
 
-	has_attached_file :picture1, styles: {medium: "250x250#"}
+	has_attached_file :picture1, styles: {medium: "220x220#"}
   validates_attachment_content_type :picture1, :content_type => /\Aimage\/.*\Z/
 
-  has_attached_file :picture2, styles: {medium: "250x250#"}
+  has_attached_file :picture2, styles: {medium: "220x220#"}
   validates_attachment_content_type :picture2, :content_type => /\Aimage\/.*\Z/
 end
