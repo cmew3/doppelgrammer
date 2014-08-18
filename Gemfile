5c0a0037dc64bc09631c350880da9f2d873e0c53
David Bayon <dbayon82@googlemail.com>
2014-08-18 14:22:31 +0100

    Started styling doppelgrammer site

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'paperclip', github: 'thoughtbot/paperclip'
+gem 'aws-sdk'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.4'
 # Use postgresql as the database for Active Record
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,15 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => 'doppelgrammer',
+      :access_key_id => Rails.application.secrets.s3_access_key_id,
+      :secret_access_key => Rails.application.secrets.s3_secret_access_key_id
+    }
+  }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
