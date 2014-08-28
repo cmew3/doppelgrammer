3872a0da7747159a9a597127fc68e7b91266294b
cmew3 <c.wheeler-booth@cantab.net>
2014-08-28 16:12:35 +0100

    added some styling to new post pusher

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -10,6 +10,6 @@ channel.bind('new_post', function(data) {
 	console.log('hello')
 	var newData = {caption1: data.caption1, picture1: data.picture1, picture2: data.picture2, caption2: data.caption2, hire_charge: data.hire_charge, tag1: data.tag1, tag2: data.tag2, hire_link: data.hire_link, tag1_link: data.tag1_link, tag2_link: data.tag2_link, vote_link: data.vote_link}
    var newPost = Mustache.render($("#post-template").html(), newData);
-	$(newPost).prependTo("#posts-container");
+	$(newPost).prependTo("#posts-container").slideDown();
 });
 
